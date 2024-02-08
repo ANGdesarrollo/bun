da2a0f75fe57451cc90e26828a83d138353b8d34
Alexis Graff <alexisgraff123@gmail.com>
2024-02-08 19:08:21 -0300

    feat: Cleanup and simplify User schema
    Removed unnecessary imports and commented-out sections from the User Entity Schema, keeping only the essential ones. This change simplifies the schema and makes it easier to understand, reducing clutter and aiding development and maintenance.

--- src/Modules/Auth/Infraestructure/Schema/User.ts
+++ src/Modules/Auth/Infraestructure/Schema/User.ts
@@ -1,36 +1,4 @@
-import { Entity, EntitySchema, PrimaryKey, Property, Unique } from '@mikro-orm/core';
-import { User } from '../../Domain/Entities/User';
-
-// export const UserEntity = new EntitySchema<User>({
-//     name: 'User',
-//     properties: {
-//         _id: {
-//             type: 'uuid',
-//             primary: true,
-//             unique: 'true'
-//         },
-//         username: {
-//             type: 'string',
-//             unique: true
-//         },
-//         password: {
-//             type: 'string'
-//         },
-//         enable: {
-//             type: 'boolean',
-//             default: false
-//         },
-//         createdAt: {
-//             type: 'Date',
-//             onCreate: () => new Date(), nullable: true
-//         },
-//         updatedAt: {
-//             type: 'Date',
-//             onCreate: () => new Date(),
-//             onUpdate: () => new Date(), nullable: true
-//         }
-//     }
-// });
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 
 @Entity()
 export class UserEntity
